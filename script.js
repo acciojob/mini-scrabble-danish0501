2dafc8f554cac40a1388917a7ed73a715e49e430
danish0501 <alidanish373@gmail.com>
2023-01-15 01:02:45 +0530

    Done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your code here
+let count;
+
+document.getElementById("evaluatedText").value = "";
+
+document.addEventListener("keyup", function () {
+    count = document.getElementById("evaluatedText").value;
+    document.getElementById("letterCount").textContent = count.length;
+    console.log(count);
+});
